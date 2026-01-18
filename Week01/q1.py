12793b7a3068dbc471d72683d354fd23628fc8df
andre-gbc <101573321@georgebrown.ca>
2026-01-18 16:32:50 -0500

    q1

--- Week01/q1.py
+++ Week01/q1.py
@@ -1,4 +1,23 @@
-# Sample Coding Questions #1 Week 01
+# Sample Coding Questions 01 Week 01
 # Andre Antoine
 
+# Defining Variables: Define an array variable with elements 1, 4, 7, 9
+my_array = [1, 4, 7, 9]
 
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracketed Version:
+e = ((a - ((b ** c) // d)) + (a % c))
+
+# Formatting: Create variable and print with format function
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions: User input and manipulation
+userAge = int(input("Please enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age: {}".format(userAge))
--- Week01/q2.py
+++
@@ -1,4 +0,0 @@
-# Sample Coding Questions #2 Week 01
-# Andre Antoine
-
-
